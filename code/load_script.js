da51d51bb8f0d8a7b12d023e49342a4672d34164
EnrikosIossifidis <enrikosi013@gmail.com>
2019-06-10 17:24:56 +0200

    New index.html

--- code/load_script.js
+++ code/load_script.js
@@ -1,3 +1,14 @@
 function test() {
 	alert("TEST");
+
+	// general parameters
+	var topMar = 10;
+	var rightMar = 10;
+	var leftMar = 10;
+	var botMar = 10;
+	margin = {top: topMar, right: rightMar, bottom: botMar, left: leftMar}
+
+	var q = d3.queue();
+	q.defer(d3.json, "https://enrikosiossifidis.github.io/progproject/data/winesData.json")
+	console.log(q);
 }
